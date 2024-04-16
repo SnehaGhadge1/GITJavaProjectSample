0d053cb3feb5f14bebddf0b85c979635740a714b
SnehaGhadge1 <SnehaGhadge1@gmail.com>
2024-04-16 23:53:29 +0530

    added Register page and test class

---
+++ src/Pages/ResgisterPage.java
@@ -0,0 +1,12 @@
+package Pages;
+
+
+
+public class ResgisterPage {
+
+	int i = 100;
+	
+	public void getNumber() {
+		System.out.println("get the Number:"+i);
+	}
+}
---
+++ src/Tests/RegisterTest.java
@@ -0,0 +1,14 @@
+package Tests;
+
+import Pages.ResgisterPage;
+
+public class RegisterTest {
+
+	public static void main(String[] args) {
+
+		ResgisterPage r = new ResgisterPage();
+		r.getNumber();
+
+	}
+
+}
