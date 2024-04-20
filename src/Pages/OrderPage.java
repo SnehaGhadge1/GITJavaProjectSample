7a4244e738c962d6252097b6e589fbb152c3a95d
SnehaGhadge1 <SnehaGhadge1@gmail.com>
2024-04-21 04:22:29 +0530

    chnages done as per sneha70's review comments

--- src/Pages/OrderPage.java
+++ src/Pages/OrderPage.java
@@ -3,6 +3,8 @@ package Pages;
 public class OrderPage {
 
 	public void order() {
+		int number =1;
 		System.out.println("get the prod");
+		System.out.println(number);
 	}
 }
--- src/Tests/OrderTest.java
+++ src/Tests/OrderTest.java
@@ -5,4 +5,5 @@ public class OrderTest {
 	int price = 888;
 	String prodName = "TV";
 	String customer = "naveen";
+	int modelNo = 676;
 }
