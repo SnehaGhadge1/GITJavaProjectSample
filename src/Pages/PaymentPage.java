f9fa4e203000ac721c0d26a3d2fab44733159a40
SnehaGhadge1 <SnehaGhadge1@gmail.com>
2024-04-21 06:28:49 +0530

    Added payment classes

---
+++ src/Pages/PaymentPage.java
@@ -0,0 +1,8 @@
+package Pages;
+
+public class PaymentPage {
+
+	public void paymentPage() {
+		System.out.println("get the paymnet");
+	}
+}
---
+++ src/Tests/PaymentTest.java
@@ -0,0 +1,11 @@
+package Tests;
+
+public class PaymentTest {
+
+	String paymnetOption = "UPI";
+	public static void main(String[] args) {
+		
+
+	}
+
+}
