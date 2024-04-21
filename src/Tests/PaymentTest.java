6324b94dc3cc32c262bcfbd2b3b7b8b809a82b6f
SnehaGhadge1 <SnehaGhadge1@gmail.com>
2024-04-21 07:14:37 +0530

    added paymnet chnages as per sneha70 review

--- src/Tests/PaymentTest.java
+++ src/Tests/PaymentTest.java
@@ -3,6 +3,9 @@ package Tests;
 public class PaymentTest {
 
 	String paymnetOption = "UPI";
+	String paymnetOption1 = "Card";
+	String paymnetOption2 = "Gpay";
+	
 	public static void main(String[] args) {
 		
 
